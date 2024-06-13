5af1827aa556855b5540c2e89cb86248ebc250de
tede0 <max.lyskov1@gmail.com>
2024-06-13 19:32:26 +0100

    added terminal and comment plugins

--- lua/tede0/remap.lua
+++ lua/tede0/remap.lua
@@ -1,4 +1,6 @@
 vim.g.mapleader = " "
+
+-- Filetree
 vim.keymap.set("n", "-", "<CMD>Oil<CR>")
 
 -- Navigation
@@ -7,9 +9,10 @@ vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 
--- Move block of code
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- Terminal
+vim.keymap.set("n", "<leader>tt", ":ToggleTerm<CR>", { desc = "Toggle terminal" })
+vim.keymap.set("t", "<C-q>", "<cmd>close<CR>", { desc = "Close terminal" })
+
 
 
 -- Ease of use
@@ -22,6 +25,9 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("n", "<Esc>", "<CMD> noh <CR>")                                           -- clear highlights
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]) -- rename
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
