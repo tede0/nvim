baf9cac2f10bc2612fd4fa2062570dc3f5c58c46
tede0 <max.lyskov1@gmail.com>
2024-06-13 22:06:35 +0100

    oil config

--- lua/tede0/lazy/oil.lua
+++ lua/tede0/lazy/oil.lua
@@ -2,7 +2,18 @@ return {
   {
     'stevearc/oil.nvim',
     opts = {},
-    -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("oil").setup({
+        delete_to_trash = true, -- e ~/.Trash | requires full disc access for terminal
+        skip_confirm_for_simple_edits = true,
+        view_options = {
+          show_hidden = true,
+          is_always_hidden = function(name, _)
+            return name == '..' or name == '.git'
+          end
+        }
+      })
+    end
   }
 }
