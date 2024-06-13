fde10a8698929b3eb9dc0c8754eaac2e06466797
tede0 <max.lyskov1@gmail.com>
2024-06-13 23:16:13 +0100

    copy error message to clipboard

--- lua/tede0/init.lua
+++ lua/tede0/init.lua
@@ -33,6 +33,18 @@ autocmd('LspAttach', {
   group = DefaultGroup,
   callback = function(e)
     local opts = { buffer = e.buf }
+
+    local function copy_diagnostic_to_clipboard()
+      local line_diagnostics = vim.diagnostic.get(e.buf, { lnum = vim.fn.line('.') - 1 })
+      if #line_diagnostics == 0 then
+        print("No diagnostics at the current line")
+        return
+      end
+      local diagnostic_message = line_diagnostics[1].message
+      vim.fn.setreg('+', diagnostic_message)
+      print("Diagnostic copied to clipboard")
+    end
+
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "gr", function() require('telescope.builtin').lsp_references() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -43,6 +55,8 @@ autocmd('LspAttach', {
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+
+    vim.keymap.set("n", "<leader>vc", copy_diagnostic_to_clipboard, opts) -- copy error message to clipboard
   end
 })
 
