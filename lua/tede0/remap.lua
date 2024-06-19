79279527721908f7606925098d2d807ff788de0f
Maksym Lyskov <maksym@lineupnow.com>
2024-06-19 13:45:15 +0100

    added tab mappings and sql dadbod

--- lua/tede0/remap.lua
+++ lua/tede0/remap.lua
@@ -1,39 +1,46 @@
+local set = vim.keymap.set
 vim.g.mapleader = " "
 
+
 -- Filetree
-vim.keymap.set("n", "-", "<CMD>Oil<CR>")
+set("n", "-", "<CMD>Oil<CR>")
 
 -- Navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
+set("n", "<C-h>", "<C-w>h")
+set("n", "<C-l>", "<C-w>l")
+set("n", "<C-j>", "<C-w>j")
+set("n", "<C-k>", "<C-w>k")
 
 -- Terminal
-vim.keymap.set("n", "<leader>tt", ":ToggleTerm<CR>", { desc = "Toggle terminal" })
-vim.keymap.set("t", "<C-q>", "<cmd>close<CR>", { desc = "Close terminal" })
+set("n", "<leader>tt", ":ToggleTerm<CR>", { desc = "Toggle terminal" })
+set("t", "<C-q>", "<cmd>close<CR>", { desc = "Close terminal" })
 
 -- Move block of code
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+set("v", "J", ":m '>+1<CR>gv=gv")
+set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Ease of use
-vim.keymap.set("n", "<leader>vs", "<C-w>v")      -- vertical split
-vim.keymap.set("n", "<leader>hs", "<C-w>s")      -- horizontal split
-vim.keymap.set("n", "<leader>x", "<cmd> x <CR>") -- close buffer
+set("n", "<leader>vs", "<C-w>v")      -- vertical split
+set("n", "<leader>hs", "<C-w>s")      -- horizontal split
+set("n", "<leader>x", "<cmd> x <CR>") -- close buffer
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+-- Tabs
+set("n", "<leader>tt", "<cmd>tabnew<CR>")
+set("n", "<TAB>", "gt")
+set("n", "<S-TAB>", "gT")
 
-vim.keymap.set("n", "<Esc>", "<CMD> noh <CR>")                                           -- clear highlights
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]) -- rename
+set("n", "J", "mzJ`z")
+set("n", "<C-d>", "<C-d>zz")
+set("n", "<C-u>", "<C-u>zz")
+set("n", "n", "nzzzv")
+set("n", "N", "Nzzzv")
 
-vim.keymap.set("x", "<leader>p", [["_dP]])
+set("n", "<Esc>", "<CMD> noh <CR>")                                           -- clear highlights
+set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]) -- rename
 
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+set("x", "<leader>p", [["_dP]])
 
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+set({ "n", "v" }, "<leader>y", [["+y]])
+set("n", "<leader>Y", [["+Y]])
+
+set({ "n", "v" }, "<leader>d", [["_d]])
